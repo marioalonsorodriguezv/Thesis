57e7d162b5c3a9b17a1fcfdc5597a47791f2fa65
Mario Alonso Rodriguez Vigueras <marioalonsorodriguezv@gmail.com>
2017-03-13 22:50:54 +0100

    juntar variables de control

--- Firstscript.R
+++ Firstscript.R
@@ -6,6 +6,8 @@ library(xlsx)
 library(repmis)
 library(tidyr)
 library(WDI)
+library(XML)
+library(plyr)
 
 possible_dir <- c('/Users/alvarolopezguiresse/GoogleDrive/[] ADMINISTRACION PUBLICA/THESIS/Thesis', '/Users/mariorodriguez/Desktop/Thesis')
 repmis::set_valid_wd(possible_dir)
@@ -67,9 +69,9 @@ QoGts<-QoGts[!(QoGts$year==2016),]
 
 ####Abrir la base de IDEA con control of corruption
 
-IDEA <- read.xlsx("graficas_y_analisis_v2.xlsx", 1)
+IDEA <- read.csv("AnalisisR.csv")
 
-IDEA$ccodealp <- countrycode(IDEA$country, 'country.name', 'iso3c', warn = FALSE)
+IDEA$ccodealp <- countrycode(IDEA$Country, 'country.name', 'iso3c', warn = FALSE)
 
 IDEA <- IDEA[, -2]
 
@@ -105,20 +107,96 @@ fixed <- plm(wb_coc ~ idea_index, data=MergedSelect, index=c("country", "year"),
 
 summary(fixed)
 
+##############Analisis de R
+
+
+####Abrimos última versión de la base de IDEA y le agregamos el iso3c
+
+IDEA <- read.csv("AnalisisR.csv")
+
+ncol(IDEA)
+
+IDEA <- IDEA[, -(39:41)]
+
+colnames(IDEA)[1] <- "country"
+
+IDEA$iso3c <- countrycode(IDEA$country, 'country.name', 'iso3c', warn = TRUE)
+
 ####Abrir datos del WEF-Judicial Independence
 
 WEFJI <- read.xlsx("WEFJudInd.xlsx", 1)
 
-
-#####Como limpiamos 
+#####Limpiar la del base del WEF y agregamos el iso3c
 
 WEFJIclean <- gather(WEFJI, country, wefji, Albania:Zimbabwe)
+  
+WEFJIclean$iso3c <- countrycode(WEFJIclean$country, 'country.name', 'iso3c', warn = TRUE)
 
 WEFJI <- write.csv(WEFJIclean, "WEFJudicialIndependence.csv")
 
 WEFJI <- read.csv("WEFJudicialIndependence.csv")
-#####Descargar datos del world bank
 
-WorldBank <- WDI(country = 'all', start = '1996', end = '2015', indicator = c('SI.POV.GINI', 'SP.DYN.LE00.IN', 'NY.GDP.MKTP.CD'), extra = FALSE)
+WEFJI <- WEFJI[,-(1)] 
 
+WEFJI <- WEFJI[,-(2)] 
 
+#####Descargar datos del world bank y limpiarlos
+
+WorldBank <- WDI(country = 'all', start = '1996', end = '2015', indicator = c('SI.POV.GINI', 'SP.DYN.LE00.IN', 'NY.GDP.MKTP.CD'), extra =TRUE)
+
+WorldBank <- WorldBank[-(1:100), ]
+
+WorldBank <- WorldBank[, (3:7)]
+
+sapply(WorldBank, class)
+
+colnames(WorldBank)[2] <- "gini"
+colnames(WorldBank)[3] <- "lifeexpectancy"
+colnames(WorldBank)[4] <- "GDP"
+
+#####Abrir la base de datos de la Cepal
+
+CepalGastos <- read.csv("CepalGastos.csv")
+
+colnames(CepalGastos)[8] <- "1996"
+colnames(CepalGastos)[9] <- "1997"
+colnames(CepalGastos)[10] <- "1998"
+colnames(CepalGastos)[11] <- "1999"
+colnames(CepalGastos)[12] <- "2000"
+colnames(CepalGastos)[13] <- "2001"
+colnames(CepalGastos)[14] <- "2002"
+colnames(CepalGastos)[15] <- "2003"
+colnames(CepalGastos)[16] <- "2004"
+colnames(CepalGastos)[17] <- "2005"
+colnames(CepalGastos)[18] <- "2006"
+colnames(CepalGastos)[19] <- "2007"
+colnames(CepalGastos)[20] <- "2008"
+colnames(CepalGastos)[21] <- "2009"
+colnames(CepalGastos)[22] <- "2010"
+colnames(CepalGastos)[23] <- "2011"
+colnames(CepalGastos)[24] <- "2012"
+colnames(CepalGastos)[25] <- "2013"
+colnames(CepalGastos)[26] <- "2014"
+colnames(CepalGastos)[27] <- "2015"
+
+CepalGastos <- CepalGastos[,-(2:7)] 
+
+CepalGastos <- gather(CepalGastos, country, CepalGastos)
+
+colnames(CepalGastos)[2] <- "year"
+
+CepalGastos$iso3c <- countrycode(CepalGastos$country, 'country.name', 'iso3c', warn = TRUE)
+
+CepalGastos <- CepalGastos[,-(1)] 
+
+####Juntar las cuatro bases de datos 
+
+AnalisisRCompleta <- merge(IDEA, WorldBank, by = c('iso3c', 'year'))
+
+AnalisisRCompleta <- merge(AnalisisRCompleta, WEFJI, by = c('iso3c', 'year'))
+
+write.csv(AnalisisRCompleta, file = "AnalisisRCompleta.csv")
+
+AnalisisAL <- merge(AnalisisRCompleta, CepalGastos, by = c('iso3c', 'year'))
+
+write.csv(AnalisisAL, file = "AnalisisAL.csv")
